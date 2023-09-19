fd673a0cdf6377be39792ff787e211096be6c613
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-09-19 17:56:24 -0400

    succesful fetching

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("Hello World")
+
+let apiUrl = "https://www.balldontlie.io/api/v1/teams"
+
+response = fetch(apiUrl)
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+    
