114aeb831a6997c41b64e2c7ffe023f770dd4b3e
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-10-13 19:04:37 -0400

    complete project

--- main.js
+++ main.js
@@ -1,9 +1,55 @@
-console.log("Hello World")
 
 let apiUrl = "https://www.balldontlie.io/api/v1/teams"
 
-response = fetch(apiUrl)
+function display(input){
+    fetch(apiUrl)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        let parent = document.getElementById('allCards')
+        parent.innerHTML = ''
 
-    
+        data.data.forEach(e => {  
+        let cards = document.createElement('div')
+        
+        if (input == e.full_name){
+            cards.setAttribute("id", "cards")
+            cards.innerHTML = `
+            <h1>Team Name: ${e.full_name}</h1>
+            <p>City: ${e.city}</p>
+            <p>Division: ${e.divison}</p>
+            <p>Abreviation: ${e.abbreviation}</p>
+            `
+            
+            
+            document.querySelector('.card-holders').appendChild(cards)
+        }
+        else if(input == null){
+            cards.setAttribute("id", "cards")
+            cards.innerHTML = `
+            <h1>Team Name: ${e.full_name}</h1>
+            <p>City: ${e.city}</p>
+            <p>Division: ${e.divison}</p>
+            <p>Abreviation: ${e.abbreviation}</p>
+            `
+            
+            
+            document.querySelector('.card-holders').appendChild(cards)
+        }  
+    })}
+    )
+    }
+
+const searchForm = document.getElementById("search-form")
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    input = document.getElementById("input")
+    display(input.value)
+})
+
+const showAll = document.getElementById("showAll")
+
+showAll.addEventListener('click', () => display())
+
+document.addEventListener('DOMContentLoaded', display())
